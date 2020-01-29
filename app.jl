2961caff7b34daea68d8ef56d9af05ef53eab545
Alex <globophobe@gmail.com>
2020-01-29 23:30:51 +0900

    using Dashboards

--- app.jl
+++ app.jl
@@ -1,8 +1,27 @@
-using HTTP
+import HTTP
+using Dashboards
 
 port = get(ENV, "PORT", 8080)
 port = port isa String ? parse(Int64, port) : port
 
-HTTP.listen("0.0.0.0", port, verbose = true) do http
-  write(http,  "OK")
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+app = Dash("Test app", external_stylesheets=external_stylesheets) do
+    html_div() do
+        html_h1("Hello Dashboards"),
+        html_div("Dashboards: Julia interface for Dash"),
+        dcc_graph(
+            id = "example-graph",
+            figure = (
+                data = [
+                    (x = [1, 2, 3], y = [4, 1, 2], type = "bar", name = "SF"),
+                    (x = [1, 2, 3], y = [2, 4, 5], type = "bar", name = "Montréal"),
+                ],
+                layout = (title = "Dash Data Visualization",)
+            )
+        )
+    end
 end
+
+handler = make_handler(app, debug = true)
+HTTP.serve(handler, "0.0.0.0", port)
